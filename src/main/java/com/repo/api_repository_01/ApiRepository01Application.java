17c13bec89530128c89ae6562d8a0b59e006629a
Mirko Di Cristina <mirko7726@gmail.com>
2022-10-19 15:29:42 +0200

    First commit

---
+++ src/main/java/com/repo/api_repository_01/ApiRepository01Application.java
@@ -0,0 +1,13 @@
+package com.repo.api_repository_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRepository01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRepository01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/repo/api_repository_01/entities/Car.java
@@ -0,0 +1,50 @@
+package com.repo.api_repository_01.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String modelName;
+    @Column(nullable = false)
+    private String serialNumber;
+    private double currentPrice;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+}
